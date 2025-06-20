c4d8b77d677b08bd47a944a3ebc9e5644f1e27fc
Nitin Sandhar <sandhar.nitin2@gmail.com>
2025-06-20 11:39:53 -0700

    p5js

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ function setup() //called once when the program starts
 
 function draw() //this runs in a loop to update the display
 {
-
+    point(150,250);
+    line(0,0,400,400); /*x1 and y1 is the first point of the line, 
+    and then x2 and y2 is the ending point of the line */
+    rect(0,0, 100, 100);
+    rect(100,100, 100, 100);
+    rect(0,0, 100, 100);
+    rect(0,0, 100, 100);
 }
